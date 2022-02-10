fc0d0d1951ae0f6493da799c039a6a4fea1d947f
bishwajit70 <mailtobishwa15@gmail.com>
2022-02-10 15:23:07 +0600

    Bank account completed

--- js/banking.js
+++ js/banking.js
@@ -13,19 +13,46 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const newDepositTotal = previousDepositAmount + newDepositAmount;
     depositTotal.innerText = newDepositTotal;
 
-    // update Account Balance 
-    // const balanceTotal = document.getElementById('balance-total');
 
-    // const balanceTotalText = balanceTotal.innerText;
-    // console.log(balanceTotalText);
-    // // console.log(balanceTotalText);
-    // // const previousBalanceTotal = parseFloat(balanceTotalText);
-    // // const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-    // // balanceTotal = newBalanceTotal.innerText;
+    // update Account Balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
 
 
     // clear the deposit input field 
 
     depositInput.value = '';
 
-})
+}); 
+
+// handle withdraw handler event
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    console.log(previousWithdrawTotal);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+    // clear withdraw Input 
+    withdrawInput.value = '';
+
+    // update Balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    // console.log(balanceTotalText);
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
+    balanceTotal.innerText = newBalanceTotal;
+});
